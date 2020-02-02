ee50a7297cb47bed7a4fa328700ac1998faa2863
ista2000 <mishra.istasis@gmail.com>
2020-02-02 10:11:15 +0530

    Added animation boilerplates

---
+++ DP series/1 Intro to DP/K_animations.js
@@ -0,0 +1,16 @@
+import {Animation} from '../../Manim.three.js/index.js';
+import {draw, checkpoint, delay, fill, graphTransform, undraw, unfill,animateArray, removeArray, showGraph, removeGraph, graphTransformParametricCartesian, graphTransformCartesianParametric, graphTransformParametric, showArrow, removeArrow} from "../../Manim.three.js/animations.js";
+
+
+(async () => {
+    let anim = new Animation();
+    let mesh_array = await anim.addText("Kshitijaa", '#ffffff', 18, -1, 1);
+    for(let i = 0; i < mesh_array.length; i++)
+        anim.addAnimation(draw(mesh_array[i], 200000));
+    anim.addAnimation(checkpoint());
+    anim.addAnimation(delay());
+    anim.addAnimation(checkpoint());
+    for(let i = 0; i < mesh_array.length; i++)
+        anim.addAnimation(undraw({shape: mesh_array[i]}, 200000));
+    anim.play();
+})();
